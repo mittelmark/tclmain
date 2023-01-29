187e66bdf1002a599a1594dba891515767accfde
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-01-29 18:50:53 +0100

    adding subdirectoy feature

--- tclinstall.tcl
+++ tclinstall.tcl
@@ -1,17 +1,22 @@
 #!/usr/bin/env tclsh
 
+# WIP - not ready to use
+
 if {[llength $argv] < 1 && [file tail [lindex $argv 0]] ne "setup.tcl"} {
     puts "Usage: $argv0 setup.tcl"
     puts "where setup.tcl is in the parent folder of the package"
     exit 0
 }
-
-if {[file exists [lindex $argv 0]]} {
+set setupfile [lindex $argv 0]
+if {[file exists $setupfile]} {
     source [lindex $argv 0]
 } else {
-    puts "Error: File '[lindex $argv 0]' does not exists!"
+    puts "Error: File '$setupfile' does not exists!"
     exit 0
 }
+
+set owd [pwd]
+cd [file dirname $setupfile]
 set files [list]
 foreach f $setup(include) {
     foreach file [glob -nocomplain $f] {
@@ -59,3 +64,17 @@ lappend auto_path [file join $targetdir ..]
 package require $setup(name)
 
 puts "Success: package $setup(name) [package present $setup(name)] is installed!"  
+
+if {false} {
+    # zip files in folder "config" to file test.zip
+    package require zipfile::mkzip
+    zipfile::mkzip::mkzip test.zip -directory config
+    # unzip file test.zip to folder config
+    package require zipfile::decode
+    ::zipfile::decode::open test.zip
+    set archiveDict [::zipfile::decode::archive]
+    ::zipfile::decode::unzip $archiveDict config
+    ::zipfile::decode::close
+}
+
+cd $owd
