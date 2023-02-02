b94d3a90221ccde0682a2d2261b6a643f938dc80
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-02-02 08:43:43 +0100

    version tclinstall update to 0.2.0

--- lib/tclinstall/pkgIndex.tcl
+++ lib/tclinstall/pkgIndex.tcl
@@ -1,3 +1,3 @@
 if {![package vsatisfies [package provide Tcl] 8.6]} {return}
-package ifneeded tclinstall 0.1 [list source [file join $dir tclinstall.tcl]]
+package ifneeded tclinstall 0.2.0 [list source [file join $dir tclinstall.tcl]]
 
